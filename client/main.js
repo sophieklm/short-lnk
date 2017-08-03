73f0707dbfe987bfe78ad821bdfe941d10790830
sophieklm <sklm@live.co.uk>
2017-08-03 22:15:09 +0100

    adds signup class

---
+++ client/main.js
@@ -0,0 +1,9 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Signup from './../imports/ui/Signup';
+
+Meteor.startup(() => {
+  ReactDOM.render(<Signup/>, document.getElementById('app'));
+});
