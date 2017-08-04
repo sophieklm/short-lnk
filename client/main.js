c93b2cf46f54c92a7df5c1b007da5260b93391d8
sophieklm <sklm@live.co.uk>
2017-08-04 10:46:57 +0100

    adds routes for signup and link

--- client/main.js
+++ client/main.js
@@ -1,9 +1,20 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, browserHistory } from 'react-router-dom';
 
 import Signup from './../imports/ui/Signup';
+import Link from './../imports/ui/Link';
+
+const routes = (
+  <BrowserRouter history={browserHistory}>
+    <div>
+      <Route path="/signup" component={Signup}/>
+      <Route path="/link" component={Link}/>
+    </div>
+  </BrowserRouter>
+);
 
 Meteor.startup(() => {
-  ReactDOM.render(<Signup/>, document.getElementById('app'));
+  ReactDOM.render(routes, document.getElementById('app'));
 });
