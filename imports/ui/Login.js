91da2d1f180c24a100acb0f1b2394d4a00f1c49b
sophieklm <sklm@live.co.uk>
2017-08-04 15:18:00 +0100

    adds simple shema and email validation

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -26,7 +26,7 @@ export default class Login extends React.Component {
       <div>
         <h1>Login to Short Lnk</h1>
         {this.state.error ? <p>{this.state.error}</p> : undefined}
-        <form onSubmit={this.onSubmit.bind(this)}>
+        <form onSubmit={this.onSubmit.bind(this)} noValidate>
           <input type="email" ref="email" name="email" placeholder="Email"/>
           <input type="password" ref="password" name="password" placeholder="Password"/>
           <button>Login</button>
