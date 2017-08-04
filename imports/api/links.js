b568da109b839716a7a1026e3d9c2c5c122ef3d8
sophieklm <sklm@live.co.uk>
2017-08-04 20:18:49 +0100

    adds url validation

--- imports/api/links.js
+++ imports/api/links.js
@@ -1,4 +1,5 @@
 import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
 
 export const Links = new Mongo.Collection('links');
 
@@ -12,6 +13,17 @@ Meteor.methods({
   'links.insert'(url) {
     if(!this.userId) {
       throw new Meteor.Error('not-authorised');
+    }
+    try {
+      new SimpleSchema({
+        url: {
+          type: String,
+          label: 'Your link',
+          regEx: SimpleSchema.RegEx.Url
+        }
+      }).validate({url});
+    } catch (e) {
+      throw new Meteor.Error(400, e.message);
     }
     Links.insert({
       url,
