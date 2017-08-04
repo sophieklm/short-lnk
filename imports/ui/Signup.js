ea04b879d90bfbcb0f70a812e7a7c622a625d509
sophieklm <sklm@live.co.uk>
2017-08-04 11:50:05 +0100

    playing with state

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,10 +2,31 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: this.props.count || 0
+    };
+  }
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+  decrement() {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
   render() {
     return (
       <div>
         <h1>Signup to Short Lnk</h1>
+        <p>{this.state.count}</p>
+          <div>
+            <button onClick={this.increment.bind(this)}>+1</button>
+            <button onClick={this.decrement.bind(this)}>-1</button>
+          </div>
         <Link to="/">Have an account?</Link>
       </div>
     );
