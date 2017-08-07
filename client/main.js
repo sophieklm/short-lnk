91ad831c17eeb45c76be74d31ee29e33bdfc0d01
sophieklm <sklm@live.co.uk>
2017-08-07 18:35:24 +0100

    adds session to filter visible links

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Tracker } from 'meteor/tracker';
+import { Session } from 'meteor/session';
 
 import { routes, onAuthChange } from '../imports/routes/routes';
 import '../imports/startup/simpl-schema-config.js';
@@ -10,6 +11,8 @@ Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
   onAuthChange(isAuthenticated);
 });
+
+Session.set('showVisible', true);
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
