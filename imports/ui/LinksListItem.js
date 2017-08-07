03cdf1bddd9ed388285148ab3c5daab9460a60f6
sophieklm <sklm@live.co.uk>
2017-08-07 17:59:09 +0100

    adds clipboard functionality with feedback

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copied: false
+    };
+  }
+  componentDidMount() {
+    this.clipboard = new Clipboard(this.refs.copy);
+    this.clipboard.on('success', () => {
+      this.setState({copied: true});
+      setTimeout(() => this.setState({copied: false}), 1000);
+    }).on('error', () => {
+      alert('Unable to copy');
+    });
+  }
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
   render() {
     return (
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl}>{this.state.copied ? "Copied" : "Copy"}</button>
       </div>
     );
   }
