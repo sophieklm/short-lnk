418dd3c58b585c6253a9f7af21f3788867330b72
sophieklm <sklm@live.co.uk>
2017-08-08 18:09:20 +0100

    adds more styling and not found link

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -32,7 +32,7 @@ export default class Signup extends React.Component {
         <div className="boxed-view__box">
           <h1>Signup to Short Lnk</h1>
           {this.state.error ? <p>{this.state.error}</p> : undefined}
-          <form onSubmit={this.onSubmit.bind(this)} noValidate>
+          <form className="boxed-view__form" onSubmit={this.onSubmit.bind(this)} noValidate>
             <input type="email" ref="email" name="email" placeholder="Email"/>
             <input type="password" ref="password" name="password" placeholder="Password"/>
             <button>Create Account</button>
