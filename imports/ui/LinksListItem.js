9254d5358b954a4c9442ed096ad8c70766dfc7a8
sophieklm <sklm@live.co.uk>
2017-08-08 17:14:56 +0100

    adds visitor time formatting

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Clipboard from 'clipboard';
+import moment from 'moment';
 
 export default class LinksListItem extends React.Component {
   constructor(props) {
@@ -21,13 +22,20 @@ export default class LinksListItem extends React.Component {
   componentWillUnmount() {
     this.clipboard.destroy();
   }
+  renderStats() {
+    const visitMessage = this.props.visitedCount === 1 ? 'visit' : 'visits';
+    let visitedMessage = null;
+    if (typeof this.props.lastVisitedAt === 'number') {
+      visitedMessage = `(visited ${moment(this.props.lastVisitedAt).fromNow()})`;
+    }
+    return <p>{this.props.visitedCount} {visitMessage} {visitedMessage}</p>;
+  }
   render() {
     return (
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
-        <p>{this.props.visitedCount}</p>
-        <p>{this.props.lastVisitedAt}</p>
+        {this.renderStats()}
         <button ref="copy" data-clipboard-text={this.props.shortUrl}>{this.state.copied ? "Copied" : "Copy"}</button>
         <button onClick={() => {
           Meteor.call('links.setVisibility', this.props._id, !this.props.visible);
