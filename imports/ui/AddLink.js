b0c439a9706eb902eb0a9968ba94254a7b154579
sophieklm <sklm@live.co.uk>
2017-08-08 17:22:23 +0100

    adds addlink modal setup

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import Modal from 'react-modal';
 
 export default class AddLink extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      url: ''
+      url: '',
+      isOpen: false
     };
   }
   onSubmit(e) {
@@ -30,16 +32,20 @@ export default class AddLink extends React.Component {
   render() {
     return (
       <div>
-        <p>Add Link</p>
-        <form onSubmit={this.onSubmit.bind(this)}>
-          <input
-            type="text"
-            ref="url"
-            placeholder="URL"
-            value={this.state.url}
-            onChange={this.onChange.bind(this)}/>
-          <button>Add Link</button>
-        </form>
+        <button onClick={() => this.setState({isOpen: true})}>+ Add Link</button>
+        <Modal isOpen={this.state.isOpen} contentLabel="Add Link">
+          <p>Add Link</p>
+          <form onSubmit={this.onSubmit.bind(this)}>
+            <input
+              type="text"
+              ref="url"
+              placeholder="URL"
+              value={this.state.url}
+              onChange={this.onChange.bind(this)}/>
+            <button>Add Link</button>
+          </form>
+          <button onClick={() => this.setState({isOpen: false, url: ''})}>Cancel</button>
+        </Modal>
       </div>
     );
   }
