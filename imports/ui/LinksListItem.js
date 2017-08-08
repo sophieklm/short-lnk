ead6433084cdcaf74bdb1966c2f8f3aebc0a72b0
sophieklm <sklm@live.co.uk>
2017-08-08 17:35:20 +0100

    adds visit button

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -36,6 +36,9 @@ export default class LinksListItem extends React.Component {
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
         {this.renderStats()}
+        <a href={this.props.shortUrl} target="_blank">
+          Visit
+        </a>
         <button ref="copy" data-clipboard-text={this.props.shortUrl}>{this.state.copied ? "Copied" : "Copy"}</button>
         <button onClick={() => {
           Meteor.call('links.setVisibility', this.props._id, !this.props.visible);
